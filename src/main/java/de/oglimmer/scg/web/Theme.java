2cfe5c00c9e617606f82be17d8159e447cccb017
Oli Zimpasser <oglimmer@gmail.com>
2014-02-18 22:44:53 +0100

    Changed exception handling in Theme

--- src/main/java/de/oglimmer/scg/web/Theme.java
+++ src/main/java/de/oglimmer/scg/web/Theme.java
@@ -4,22 +4,28 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 
-import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 public enum Theme {
 	INSTANCE;
 
 	private Properties prop = new Properties();
 
-	@SneakyThrows(value = IOException.class)
 	private Theme() {
 		loadTheme();
 	}
 
-	private void loadTheme() throws IOException {
+	private void loadTheme() {
 		String themeName = ScgProperties.INSTANCE.getTheme();
 		try (InputStream is = this.getClass().getResourceAsStream("/" + themeName + ".theme")) {
-			prop.load(is);
+			if (is != null) {
+				prop.load(is);
+			} else {
+				log.error("Failed to load theme=" + themeName);
+			}
+		} catch (IOException e) {
+			log.error("Failed to load theme=" + themeName, e);
 		}
 	}
 
