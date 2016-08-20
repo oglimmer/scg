1ad52a601545e4320fc5c977ed60b168ab6ebc06
Oli Zimpasser <oglimmer@gmail.com>
2016-08-20 21:44:46 +0200

    Move to SPI-based slf4j configurator

---
+++ src/main/java/de/oglimmer/scg/log/Configurator.java
@@ -0,0 +1,11 @@
+package de.oglimmer.scg.log;
+
+import de.oglimmer.utils.BaseConfigurator;
+
+public class Configurator extends BaseConfigurator {
+
+	public Configurator() {
+		super("scg");
+	}
+
+}
